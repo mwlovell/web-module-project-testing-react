3af5631da71673ef97503e5dae796c62e845e998
Mike Lovell <mike@Carlys-MacBook-Air.local>
2022-04-25 20:11:49 -0700

    added project started testepisode

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,22 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const testEpisode = {
+    id:1,
+    name:'',
+    image:"http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
+    season: 1,
+    number: 1,
+    summary: "test summary",
+    runtime: 1
+}
 
-test("renders the summary test passed as prop", () => { });
+test("renders without error", () => { 
+    render(<Episode episode={testEpisode}/>)
+});
+
+test("renders the summary test passed as prop", () => {
+   
+ });
 
 test("renders default image when image is not defined", () => { });
